711b641d0c25bb824260269cfbf48896aa24b9a7
Joao <joaojoao210933@gmail.com>
2026-05-25 00:38:32 -0300

    Ajuste do host e porta para o deploy

--- app.py
+++ app.py
@@ -159,4 +159,6 @@ def logout():
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Lê a porta que a Render oferece ou usa a 5000 por padrão se for no PC
+    porta = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=porta)
