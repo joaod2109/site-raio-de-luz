23500d2fdeead8f0acb3b6139db05789aea44732
Joao <joaojoao210933@gmail.com>
2026-05-25 00:55:47 -0300

    Ajuste do host e porta para o deploy

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.config["MAIL_USE_TLS"]        = True
 app.config["MAIL_USERNAME"]       = os.environ.get("MAIL_USERNAME", "comcatraiodeluz@gmail.com")
 
 # ⚠️ ATENÇÃO: Substitui as letras abaixo pelas 16 letras da Senha de App que vais gerar na Google
-app.config["MAIL_PASSWORD"]       = os.environ.get("MAIL_PASSWORD", "xedz znkp ftxp bdov") 
+app.config["MAIL_PASSWORD"]       = os.environ.get("MAIL_PASSWORD", "fnmn nlxm cuai plnv") 
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_USERNAME", "comcatraiodeluz@gmail.com")
 
 mail = Mail(app)
